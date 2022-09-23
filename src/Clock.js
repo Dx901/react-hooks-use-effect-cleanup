058f359d900658c51c0978735cc1efdda2e9d31f
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-09-23 13:27:25 +0300

    Done

--- src/Clock.js
+++ src/Clock.js
@@ -3,11 +3,15 @@ import React, { useState, useEffect } from "react";
 function Clock() {
   const [time, setTime] = useState(new Date());
 
-  useEffect(() => {
-    setInterval(() => {
+  useEffect(()=> {
+    const timerID = setInterval(() => {
       setTime(new Date());
     }, 1000);
-  }, []);
+
+    return function cleanup() {
+      clearInterval(timerID);
+    }
+  }, [])
 
   return <div>{time.toString()}</div>;
 }
